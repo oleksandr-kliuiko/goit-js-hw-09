74a529e990d9d7a6a3be7b45cf90f905877dbb4f
Oleksandr Kliuiko <oleksandr.kliuiko5804@gmail.com>
2026-08-23 20:28:11 +0300

    hw-9

---
+++ src/js/2-form.js
@@ -0,0 +1,42 @@
+const form = document.querySelector(".feedback-form");
+
+const formData = {
+  email: "",
+  message: "",
+};
+
+form.addEventListener("input", event => {
+  formData[event.target.name] = event.target.value.trim();
+
+  localStorage.setItem("feedback-form-state", JSON.stringify(formData));
+});
+
+const savedData = localStorage.getItem("feedback-form-state");
+
+if (savedData) {
+  const parsedData = JSON.parse(savedData);
+
+  formData.email = parsedData.email;
+  formData.message = parsedData.message;
+
+  form.elements.email.value = formData.email;
+  form.elements.message.value = formData.message;
+}
+
+form.addEventListener("submit", event => {
+  event.preventDefault();
+
+  if (formData.email === "" || formData.message === "") {
+    alert("Fill please all fields");
+    return;
+  }
+
+  console.log(formData);
+
+  localStorage.removeItem("feedback-form-state");
+
+  formData.email = "";
+  formData.message = "";
+
+  form.reset();
+});
